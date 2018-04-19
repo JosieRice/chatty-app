342847b40fbc6bc85350a6c499805bb36078fccf
Craig Rice <craigricetong@gmail.com>
2018-04-19 02:33:29 +0000

    refactored chatbar component

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -5,51 +5,32 @@ class ChatBar extends Component {
     super();
     // for storing last updated username
     this.state = {
-      username: 'Anonymous'
-      // content: ''
+      username: 'Anonymous',
     };
     this.onKeyPress = this.onKeyPress.bind(this);
-    this.onContentChange = this.onContentChange.bind(this);
-    this.onUsernameChange = this.onUsernameChange.bind(this);
     this.usernameEntered = this.usernameEntered.bind(this);
   }
 
-
-
-
   // on Enter, it passes the username and content into the postChat function
-  onKeyPress (event) { 
-    if (event.key === "Enter") 
-    { this.postChat(
-        document.getElementById('chat-username').value,
-        document.getElementById('chat-input').value);
+  onKeyPress (event) {
+    if (event.key === "Enter") {
+      const username = document.getElementById('chat-username').value;
+      this.props.onPostChat(
+        username,
+        document.getElementById('chat-input').value
+      );
+      // clears message input field
       event.target.value = '';
+      // sets state username if it's new
+      if (this.state.username !== username) {
+        this.setState({ username })
+      }
     } 
-  }
-
-  // onChange event for typing in username field
-  onUsernameChange (event) {
-    this.setState({
-      username: event.target
-    });
-  }
-
-  // onChange event for typing in message field
-  onContentChange (event) {
-    this.setState({
-      content : event.target
-    });
-  }
-
-  // calls the onPostChat to change state of app.jsx
-  postChat(username, content) {
-    this.props.onPostChat(username, content);
   }
 
   // sends previous and new username info to app.jsx function and sets new currentUser.
   usernameEntered (event) { 
-    if (event.key === "Enter") 
-    { 
+    if (event.key === "Enter") { 
       const username = document.getElementById('chat-username').value
       this.props.onChangeUser(username, this.state.username);
       this.setState({ username });
@@ -58,16 +39,14 @@ class ChatBar extends Component {
 
   render() {
     return (
-    <footer className="chatbar">
+      <footer className="chatbar">
         <input 
           id="chat-username" 
           className="chatbar-username" 
           placeholder="And you are...?" 
           // sends notification when a user changes their username
           onKeyPress={this.usernameEntered}
-          // takes control of the state of the input field
-          // onChange={this.onUsernameChange}
-          />
+        />
 
         <input 
           id="chat-input" 
@@ -75,10 +54,8 @@ class ChatBar extends Component {
           placeholder="Type a message and hit ENTER" 
           // enter runs the postChat function and passes the input values of username and chat content
           onKeyPress={this.onKeyPress}
-          // takes control of the state of input field
-          // onChange={this.onContentChange}
-          />
-    </footer>   
+        />
+      </footer>  
     );
   }
 }
